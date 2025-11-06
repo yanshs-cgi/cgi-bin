4ac2a419fc98c9f2de9003642774feaebde550a6
yanshs-cgi <trasersecteam@gmail.com>
2025-11-07 03:39:10 +0700

    Add files via upload

---
+++ x.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<meta charset=utf-8>
+<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
+<title>Traser Sec Team Ft ./G4l4u3rs.</title>
+<link rel=icon href=https://raw.githubusercontent.com/AlvaXPloit/ISI/refs/heads/main/IMG_20250829_022558_647.jpg>
+<link href='https://fonts.googleapis.com/css2?family=Urbanist:wght@200;300;500;700&display=swap' rel=stylesheet>
+<style>
+body{margin:0;background:#000;font-family:Urbanist,sans-serif;color:#d2d2d2}
+.wrap{position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);text-align:center;width:90%}
+.wrap img{width:165px;border-radius:18px;box-shadow:0 0 12px #111}
+h2{font-size:18px;margin:12px 0;font-weight:600;color:#cfcfcf;letter-spacing:.5px}
+.master{font-size:13px;opacity:.75;margin-top:5px;letter-spacing:.6px}
+.poem{font-size:11px;line-height:17px;opacity:.75;margin-top:12px}
+.footer{margin-top:14px;font-size:11px;opacity:.7}
+.marq{margin-top:14px;font-size:11px;opacity:.75;color:#bbb}
+</style>
+</head>
+<body>
+<div class=wrap>
+<img src=https://raw.githubusercontent.com/yanshs-cgi/cgi-bin/refs/heads/main/675ca2f774a41f7505e6513dad3382fc.jpg>
+<h2>Stamped By Traser Sec Team Ft Tiger Cyber Forc3</h2>
+<div class=master>we are called <b>Master Of WordPress</b></div>
+<div class=poem>
+This website has been taken over by the Traser Sec team <br>
+#LOL #FUN
+</div>
+<div class=footer>&gt; The King Of WordPress &lt;</div>
+<marquee class=marq>Thanks To: Anon404 - AlvaXploit - Freedom Security - Rapip Icikiwir - Always Jall - Anonpis</marquee>
+</div>
+</body>
+</html>
